e2f062e6ffa7f76e73de8d236da194bde77ea282
mohmmad anas alhussni <mohmmadanasalhussni@gmail.com>
2025-12-21 02:34:12 +0300

    Finsh Create InputValidate Libary

--- Bank Project Coures 11/Bank Project Coures 11.cpp
+++ Bank Project Coures 11/Bank Project Coures 11.cpp
@@ -2,63 +2,26 @@
 #include "clsString.h"; 
 #include "clsDate.h" ; 
 #include "clsUtitl.h" ; 
+#include "clsInputValidate.h" ;
 using namespace std; 
 int main()
 {
-	clsUtitl::Srand(); 
-	cout << clsUtitl::RandomNumber(1, 10) << endl;
-	cout << clsUtitl::GetRandomCharacter(clsUtitl::Digit) << endl; 
-	cout << clsUtitl::GenerateWord(clsUtitl::MixChars, 8) << endl;
-	cout << clsUtitl::GenerateKey(clsUtitl::MixChars) << endl;
-	 clsUtitl::GeneratesKeys(10, clsUtitl::MixChars);
-	 cout << "\n"; 
-	 //Swap int : 
-	 int x = 10, y = 20; 
-	 cout << x << "" << y << endl; 
-	 clsUtitl::Swap(x, y); 
-	 cout << x << "" << y << endl;
-	 double a = 10.5 , b = 20.5 ;
-	 cout << a << " " << b << endl; 
-	 clsUtitl::Swap(a, b); 
-	 cout << a << " " << b << endl;
-	 string s1 = "Ali", s2 = "Ahmad"; 
-	 cout << s1 << " " << s2 << endl; 
-	 clsDate d1(1, 10, 2022) , d2(1,1,2022);
-	 d1.Print(); 
-	 d2.Print(); 
-	 clsUtitl::Swap(d1, d2); 
-	 d1.Print(); 
-	 d2.Print(); 
-	 int arr1[5] = { 1,2,3,4,5 }; 
-	 clsUtitl::ShuffleArray(arr1, 5); 
-	 cout << "\nAfter Array Shuffle \n"; 
-	 for (int i = 0; i < 5; i++) {
-		 cout << arr1[i] << endl; 
-	 }
-	 string arr2[5] = { "Ali","Ahmad","Anas","Nezar","Tyesser" }; 
-	 clsUtitl::ShuffleArray(arr2 , 5);
-	 cout << "\nAfter Array Shuffle \n";
-	 for (short i = 0; i < 5; i++) {
-		 cout << arr2[i] << endl; 
-	 }
-	 int Arr3[5];
-	 clsUtitl::FillArrayWithRandomNumber(Arr3, 5, 20, 50); 
-	 cout << "\n Array After fill\n"; 
-	 for (int i = 0; i < 5; i++) {
-		 cout << Arr3[i] << endl; 
-	 }
-	 string Arr4[5]; 
-	 clsUtitl::FillArrayWithRandomWords(Arr4, 5, clsUtitl::MixChars, 8); 
-	 cout << "\n Array After fill Words\n";
-	 for (int i = 0; i < 5; i++) {
-		 cout << Arr4[i] << endl;
-	 }
-	 string Arr5[5];
-	 clsUtitl::FillArrayWithRandomKeys(Arr5, 5, clsUtitl::MixChars);
-	 cout << "\n Array After fill Keys\n";
-	 for (int i = 0; i < 5; i++) {
-		 cout << Arr5[i] << endl;
-	 }
-	 cout << "\nText1" << clsUtitl::Tabs(5) << "Text2\n"; 
+	cout << clsInputValidate::IsNumberBetween(5, 1, 10) << endl; 
+	cout << clsInputValidate::IsNumberBetween(2.5, 3.2, 4.5) << endl; 
+	cout << clsInputValidate::IsDateBetween(clsDate(), clsDate(8, 12, 2022), clsDate(31, 12, 2022)) << endl; 
+	cout << clsInputValidate::IsDateBetween(clsDate(), clsDate(31, 12, 2022) , clsDate(8, 12, 2022) ) << endl;
+	cout << "Please Enter a Number :\n"; 
+	int x = clsInputValidate::ReadIntNumber("Invaild Number,Enter agin\n"); 
+	cout << "x=" << x << endl;
+	cout << "Please Enter Number between 1 and 5 :\n"; 
+	int y = clsInputValidate::ReadIntNumberBetween(1, 5, "Number is not within range"); 
+	cout << "y=" << y << endl; 
+	cout << "Please Enter a Double Number :\n";
+	double a = clsInputValidate::ReadDblNumber("Invaild Number,Enter agin\n");
+	cout << "a=" << a<<endl;
+	cout << "Please Enter a Double Number between 1 and 5 :\n";
+	double b = clsInputValidate::ReadDblNumberBetween(1, 5, "Number is not within range"); 
+	cout << "b=" << b; 
+	cout << endl << clsInputValidate::IsValidDate(clsDate(35, 12, 2022)); 
 }
 
--- Bank Project Coures 11/clsInputValidate.h
+++ Bank Project Coures 11/clsInputValidate.h
@@ -1,5 +1,80 @@
 #pragma once
+#include "clsDate.h" ; 
+
 class clsInputValidate
 {
+public : 
+	static bool IsNumberBetween(short Number, short From, short To) {
+		if (Number >= From && Number <= To)
+		return true;
+		else
+			return false;
+	}
+	static bool IsNumberBetween(int Number, int From, int To) {
+		if (Number >= From && Number <= To)
+		return true;
+		else
+			return false;
+	}
+	static bool IsNumberBetween(double Number, double From, double To) {
+		if (Number >= From && Number <= To)
+			return true;
+		else
+			return false;
+	}
+	static bool IsDateBetween(clsDate Date, clsDate DateFrom, clsDate DateTo) {
+		if ((clsDate::IsDate1AfterDate2(Date, DateFrom) || clsDate::IsDate1EqualDate2(Date, DateFrom))
+			&&
+			(clsDate::IsDate1BeforeDate2(Date, DateTo) || clsDate::IsDate1EqualDate2(Date, DateTo))
+			) {
+			return true; 
+		}
+		if ((clsDate::IsDate1AfterDate2(Date, DateTo) || clsDate::IsDate1EqualDate2(Date, DateTo))
+			&&
+			(clsDate::IsDate1BeforeDate2(Date, DateFrom) || clsDate::IsDate1EqualDate2(Date, DateFrom))
+			) {
+			return true;
+		}
+		return false;
+	}
+	static int ReadIntNumber(string ErrorMessage="Invaild Number ,Enter agine\n") {
+		int Number; 
+		while (!(cin >> Number)) {
+			cin.clear(); 
+			cin.ignore(numeric_limits<streamsize>::max(),'\n');
+			cout << ErrorMessage; 
+		}
+		return Number; 
+	}
+	static int ReadIntNumberBetween(int From, int To, string ErrorMessage = "Number is not within range please Enter agin") {
+		int Number; 
+		Number = ReadIntNumber();
+		while (!IsNumberBetween(Number,From,To) ) {
+			cout << ErrorMessage << endl; 
+			Number = ReadIntNumber();
+		}
+		return Number; 
+	}
+	static double ReadDblNumberBetween( double From, double To, string ErrorMessage = "Number is not within range please Enter agin") {
+		double Number;
+		Number = ReadIntNumber();
+		while (!IsNumberBetween(Number, From, To)) {
+			cout << ErrorMessage << endl;
+			Number = ReadIntNumber();
+		}
+		return Number;
+	}
+	static double ReadDblNumber(string ErrorMessage = "Invaild Number ,Enter agine\n") {
+		double Number;
+		while (!(cin >> Number)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << ErrorMessage;
+		}
+		return Number;
+	}
+	static bool IsValidDate(clsDate Date) {
+		return Date.IsValidDate(); 
+	}
 };
 
