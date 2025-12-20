11cfb92f7c2f33be626d559dd5bdd9df368f7d0a
mohmmad anas alhussni <mohmmadanasalhussni@gmail.com>
2025-12-20 16:38:41 +0300

    Create Util Libary

--- Bank Project Coures 11/Bank Project Coures 11.cpp
+++ Bank Project Coures 11/Bank Project Coures 11.cpp
@@ -1,16 +1,64 @@
 #include <iostream>
 #include "clsString.h"; 
 #include "clsDate.h" ; 
+#include "clsUtitl.h" ; 
 using namespace std; 
 int main()
 {
-	clsDate Date1; 
-	Date1.Print(); 
-	clsDate Date2("31/1/2022"); 
-	Date2.Print(); 
-	clsDate Date3(1,1,2023);
-	Date3.Print(); 
-	clsDate Date4(250, 2022); 
-	Date4.Print(); 
+	clsUtitl::Srand(); 
+	cout << clsUtitl::RandomNumber(1, 10) << endl;
+	cout << clsUtitl::GetRandomCharacter(clsUtitl::Digit) << endl; 
+	cout << clsUtitl::GenerateWord(clsUtitl::MixChars, 8) << endl;
+	cout << clsUtitl::GenerateKey(clsUtitl::MixChars) << endl;
+	 clsUtitl::GeneratesKeys(10, clsUtitl::MixChars);
+	 cout << "\n"; 
+	 //Swap int : 
+	 int x = 10, y = 20; 
+	 cout << x << "" << y << endl; 
+	 clsUtitl::Swap(x, y); 
+	 cout << x << "" << y << endl;
+	 double a = 10.5 , b = 20.5 ;
+	 cout << a << " " << b << endl; 
+	 clsUtitl::Swap(a, b); 
+	 cout << a << " " << b << endl;
+	 string s1 = "Ali", s2 = "Ahmad"; 
+	 cout << s1 << " " << s2 << endl; 
+	 clsDate d1(1, 10, 2022) , d2(1,1,2022);
+	 d1.Print(); 
+	 d2.Print(); 
+	 clsUtitl::Swap(d1, d2); 
+	 d1.Print(); 
+	 d2.Print(); 
+	 int arr1[5] = { 1,2,3,4,5 }; 
+	 clsUtitl::ShuffleArray(arr1, 5); 
+	 cout << "\nAfter Array Shuffle \n"; 
+	 for (int i = 0; i < 5; i++) {
+		 cout << arr1[i] << endl; 
+	 }
+	 string arr2[5] = { "Ali","Ahmad","Anas","Nezar","Tyesser" }; 
+	 clsUtitl::ShuffleArray(arr2 , 5);
+	 cout << "\nAfter Array Shuffle \n";
+	 for (short i = 0; i < 5; i++) {
+		 cout << arr2[i] << endl; 
+	 }
+	 int Arr3[5];
+	 clsUtitl::FillArrayWithRandomNumber(Arr3, 5, 20, 50); 
+	 cout << "\n Array After fill\n"; 
+	 for (int i = 0; i < 5; i++) {
+		 cout << Arr3[i] << endl; 
+	 }
+	 string Arr4[5]; 
+	 clsUtitl::FillArrayWithRandomWords(Arr4, 5, clsUtitl::MixChars, 8); 
+	 cout << "\n Array After fill Words\n";
+	 for (int i = 0; i < 5; i++) {
+		 cout << Arr4[i] << endl;
+	 }
+	 string Arr5[5];
+	 clsUtitl::FillArrayWithRandomKeys(Arr5, 5, clsUtitl::MixChars);
+	 cout << "\n Array After fill Keys\n";
+	 for (int i = 0; i < 5; i++) {
+		 cout << Arr5[i] << endl;
+	 }
+	 cout << "\nText1" << clsUtitl::Tabs(5) << "Text2\n"; 
 }
 
---
+++ Bank Project Coures 11/clsUtitl.h
@@ -0,0 +1,150 @@
+#pragma once
+#include <iostream>
+#include <cstdlib>
+#include "clsDate.h" ; 
+class clsUtitl
+{
+public: 
+	static void Srand() {
+		srand((unsigned)time(NULL));
+	}
+
+	static int RandomNumber(int From, int To) {
+		return rand() % (To - From + 1) + From;
+	}
+	
+    enum enCharacterType {
+        SamallLetter = 1, CapitalLetter = 2,
+        Digit = 3, MixChars = 4, SpecialCharacter = 5
+    };
+  static  char GetRandomCharacter(enCharacterType CharType) {
+        if (CharType == enCharacterType::MixChars) {
+            CharType = (enCharacterType)RandomNumber(1, 3); 
+        }
+        switch (CharType)
+        {
+        case enCharacterType::SamallLetter:
+            return char(RandomNumber(97, 122));
+
+        case enCharacterType::CapitalLetter:
+            return char(RandomNumber(65, 90));
+        case enCharacterType::Digit:
+            return char(RandomNumber(48, 57));
+            break;
+        case enCharacterType::SpecialCharacter:
+            return char(RandomNumber(33, 47));
+            break;
+        }
+    }
+
+ static string GenerateWord(enCharacterType CharType, short Length) {
+      string Word = "";
+      for (int i = 1; i <= Length; i++) {
+          Word += GetRandomCharacter(CharType);
+      }
+      return Word;
+  }
+ static string GenerateKey(enCharacterType CharacterType = CapitalLetter) {
+     string Key = "";
+     Key = GenerateWord(CharacterType, 4) + "-";
+     Key = Key + GenerateWord(CharacterType, 4) + "-";
+     Key = Key + GenerateWord(CharacterType, 4) + "-";
+     Key = Key + GenerateWord(CharacterType, 4);
+     return Key;
+ }
+static void GeneratesKeys(short NumberOfKey , enCharacterType CharacterType) {
+     for (int i = 1; i <= NumberOfKey; i++) {
+         cout << "Key [" << i << "]";
+         cout << GenerateKey(CharacterType) << "\n";
+     }
+ }
+static void Swap(int& A, int& B) {
+    int Temp = 0; 
+    Temp = A; 
+    A = B; 
+    B = Temp; 
+}
+static void Swap(double& A, double& B) {
+    double Temp = 0; 
+    Temp = A; 
+    A = B; 
+    B = Temp; 
+}
+static void Swap(string& S1, string& S2) {
+    string Temp = ""; 
+    Temp = S1; 
+    S1 = S2; 
+    S2 = Temp; 
+}
+static void Swap(clsDate& Date1, clsDate& Date2) {
+    clsDate::SwapDates(Date1, Date2); 
+}
+static void ShuffleArray(int arr[100], int arrLength) {
+    for (int i = 0; i < arrLength; i++) {
+        int index1 = RandomNumber(1, arrLength) - 1;
+        int index2 = RandomNumber(1, arrLength) - 1;
+        Swap(arr[index1], arr[index2]);
+
+    }
+}
+//ArrayShuffle
+static void ShuffleArray(string arr[100], int arrLength) {
+    for (int i = 0; i < arrLength; i++) {
+        int index1 = RandomNumber(1, arrLength) - 1;
+        int index2 = RandomNumber(1, arrLength) - 1;
+        Swap(arr[index1], arr[index2]);
+
+    }
+    }
+
+static void FillArrayWithRandomNumber(int arr[100], int arrLength, short From, short To) {
+
+    for (int i = 0; i < arrLength; i++) {
+        arr[i] = RandomNumber(From, To);
+    }
+}
+static void FillArrayWithRandomWords(string  arr[100], short arrLength, enCharacterType CharacterType, short LengthWord) {
+    for (short i = 0; i < arrLength; i++) {
+        arr[i] = GenerateWord(CharacterType, LengthWord);
+    }
+}
+static void FillArrayWithRandomKeys(string arr[100] , short arrLength , enCharacterType CharacterType) {
+    for (short i = 0; i < arrLength; i++) {
+        arr[i] = GenerateKey(CharacterType);
+    }
+}
+static string Tabs(short NumberOfTabs) {
+    string tap="";
+    for (int i = 1; i <= NumberOfTabs; i++) {
+        tap += "\t";
+    }
+    return tap;
+}
+static string  EncryptText(string Text, short EncryptionKey)
+{
+
+    for (int i = 0; i <= Text.length(); i++)
+    {
+
+        Text[i] = char((int)Text[i] + EncryptionKey);
+
+    }
+
+    return Text;
+
+}
+
+static string  DecryptText(string Text, short EncryptionKey)
+{
+
+    for (int i = 0; i <= Text.length(); i++)
+    {
+
+        Text[i] = char((int)Text[i] - EncryptionKey);
+
+    }
+    return Text;
+
+}
+};
+
